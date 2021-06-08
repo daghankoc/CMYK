7f4d3d504544fc435f1f74b34329070521bd23fa
Da5iid <75045999+Da5iid@users.noreply.github.com>
2021-06-08 14:31:47 -0700

    re exported potentially corrupted .png file for the rybits.

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -59,7 +59,7 @@ class Play extends Phaser.Scene {
         });
 
         //rybit spritesheet
-        this.load.spritesheet('rybit', "./assets/powerups/rybit_spritesheet.png",{
+        this.load.spritesheet('rybit', "./assets/powerups/rybit_spritesheetFixed.png",{
             frameWidth: 86,
             frameHeight: 86,
         });
